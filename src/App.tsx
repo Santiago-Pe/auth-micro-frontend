1ac12c53783f66b55f71a617e668a3bfa766aad5
Santiago-pe <penasantiago346@gmail.com>
2024-06-10 08:24:36 -0300

    ADD: add ui components forms, first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-white py-6 sm:py-8 lg:py-12">
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-8 lg:text-3xl">
+          Login
+        </h2>
 
-export default App
+        <form className="mx-auto max-w-lg rounded-lg border">
+          <div className="flex flex-col gap-4 p-4 md:p-8">
+            <div>
+              <label
+                htmlFor="email"
+                className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+              >
+                Email
+              </label>
+              <input
+                name="email"
+                className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor="password"
+                className="mb-2 inline-block text-sm text-gray-800 sm:text-base"
+              >
+                Password
+              </label>
+              <input
+                name="password"
+                className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
+              />
+            </div>
+
+            <button className="block rounded-lg bg-gray-800 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-gray-300 transition duration-100 hover:bg-gray-700 focus-visible:ring active:bg-gray-600 md:text-base">
+              Log in
+            </button>
+          </div>
+
+          <div className="flex items-center justify-center bg-gray-100 p-4">
+            <p className="text-center text-sm text-gray-500">
+              Don't have an account?{" "}
+              <a
+                href="#"
+                className="text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
+              >
+                Register
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default App;
